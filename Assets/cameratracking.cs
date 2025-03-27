df0030a996ed7565c305cb4f5bbf06615a4eaa8e
Yk_ <bo.bolweg@gmail.com>
2025-03-27 10:54:52 +0100

    camera system

---
+++ Assets/cameratracking.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class cameratracking : MonoBehaviour
+{
+    public GameObject player;
+    public Camera cameratrackingCamera;
+    public int distancefromcharacter = 25;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
