0d840a42b0f86a83ede383ef7c3d043a8cccba19
Ykada_ <YkadaHiroka@gmail.com>
2025-04-04 12:48:23 +0200

    Update enemyhit.cs

--- Assets/enemyhit.cs
+++ Assets/enemyhit.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class enemyhit : MonoBehaviour
 {
+public string targettag;
+
     void OnCollisionEnter(Collision collision)
     {
-        // Check if the object collides with a bullet (or any object with the "Projectile" tag)
-        if (collision.gameObject.CompareTag("Projectile"))
+        // Check if the object collides with a bullet (or any object with the "Projectile" ta
+        if (collision.gameObject.CompareTag(targettag))
         {
             Destroy(gameObject);  // Destroy the object this script is attached to
         }
