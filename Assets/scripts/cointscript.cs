12a49dc986412cf9ee0b1daab3e811ccb9d668db
Yk_ <bo.bolweg@gmail.com>
2025-05-23 14:16:50 +0200

    yesy

--- Assets/scripts/cointscript.cs
+++ Assets/scripts/cointscript.cs
@@ -8,9 +8,21 @@ public class cointscript : MonoBehaviour
     public int score;
     public float time = 25f;
     public List<GameObject> Coins;
-
+    public LayerMask coinLayerMask;
+    float x;
+    float y;
+    float z;
+    Vector3 pos;
     void Start()
     {
+        foreach (GameObject coinPrefab in Coins)
+        {
+            float x = Random.Range(-5f, 5f);
+            float z = Random.Range(-5f, 5f);
+            Vector3 pos = new Vector3(x, y, z);
+
+            Instantiate(coinPrefab, pos, Quaternion.identity);
+        }
         score = 0;
         Coins = new List<GameObject>(GameObject.FindGameObjectsWithTag("Coin"));
         foreach (GameObject coin in Coins)
